4a4acf6b980396678c9fe287e550fd54878e23d4
gingerzadddy <84648295+gingerzadddy@users.noreply.github.com>
2022-05-13 14:59:22 -0400

    Update sentry.server.config.js

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -8,10 +8,10 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 const ENV = process.env.VERCEL_ENV || process.env.NODE_ENV
 
 Sentry.init({
-  dsn: SENTRY_DSN,
+  dsn:"https://88e249dc5d1f40789f126a4a16785e49@o1244462.ingest.sentry.io/6400946",
   environment: ENV === 'production' ? 'production' : 'development',
   // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 0,
+  tracesSampleRate: 1.0,
   // ...
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
